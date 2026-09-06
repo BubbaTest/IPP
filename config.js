01cec7ebca14a8d00257b34aa168196145bc57a7
BubbaTest <157048797+BubbaTest@users.noreply.github.com>
2026-09-06 08:14:58 -0600

    Change apiBase URL in config.js
    Updated the apiBase URL for the production environment.

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 /**
  * Configuración del entorno activo.
- * En IIS: reemplace este archivo o su contenido al desplegar en otro entorno. https://appserviciosbe.inide.gob.ni
+ * En IIS: reemplace este archivo o su contenido al desplegar en otro entorno. 
  * Plantilla de referencia: config.example.js https://localhost:7062
  */
 window.APP_CONFIG = {
   environment: 'production',
-  apiBase: 'https://apicapacitacion.inide.gob.ni/endpoint/cipp'
+  apiBase: 'https://apicapacitacion.ni/endpoint/cipp'
 };
